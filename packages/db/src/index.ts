1b3b2c00f2b5924dc94854b0a09df109f225bfb0
prashulkale <prashulkale@gmail.com>
2025-01-19 23:22:19 +0530

    agencyPage

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,18 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: ["info", "warn", "error"],
-  });
-};
 
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
-}
 
-const prisma: ReturnType<typeof prismaClientSingleton> =
-  globalThis.prismaGlobal ?? prismaClientSingleton();
+export const prisma = new PrismaClient()
 
-export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+// const prismaClientSingleton = () => {
+//   return new PrismaClient({
+//     log: ["info", "warn", "error"],
+//   });
+// };
+
+
+
+// declare global {
+//   var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+// }
+
+// const prisma: ReturnType<typeof prismaClientSingleton> =
+//   globalThis.prismaGlobal ?? prismaClientSingleton();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
